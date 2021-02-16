3b10e1ad0460f5f888c715eabb03cc7ecf14dbfc
seouuuukjin <uukjin4@gmail.com>
2021-02-16 22:10:23 +0900

    1~9,0,00,사칙연산 버튼 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import './App.css';
+import React from "react"
 
-function App() {
+function App({name}) {
+  const handleClick = () => {
+    alert(name);
+  }
+
   return (
-    <div className="App">hi
+  <div class = "cal">
+    <div id = "arr1">
+      <button onClick={handleClick}>1</button>
+      <button onClick={handleClick}>2</button>
+      <button onClick={handleClick}>3</button>
+      <button onClick={handleClick}>/</button>
     </div>
+    <div id = "arr2">
+      <button onClick={handleClick}>4</button>
+      <button onClick={handleClick}>5</button>
+      <button onClick={handleClick}>6</button>
+      <button onClick={handleClick}>x</button>
+    </div>
+    <div id = "arr3">
+      <button onClick={handleClick}>7</button>
+      <button onClick={handleClick}>8</button>
+      <button onClick={handleClick}>9</button>
+      <button onClick={handleClick}>-</button>
+    </div>
+    <div id = "arr4">
+      <button onClick={handleClick}>0</button>
+      <button onClick={handleClick}>00</button>
+      <button onClick={handleClick}>=</button>
+      <button onClick={handleClick}>+</button>
+    </div>
+  </div>
   );
 }
-
 export default App;
