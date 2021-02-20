1e58a7d9ea16c4a3b353aad7cca3cd8aa2e5154c
seouuuukjin <uukjin4@gmail.com>
2021-02-20 10:46:32 +0900

    버튼 마다 온클릭시마다 다른 함수를 호출시키기

--- src/App.js
+++ src/App.js
@@ -1,37 +1,70 @@
 import React from "react"
 
-function App() {
-  const handleClick = (num) => {
-    alert(num);
+class App extends React.Component {
+  state = {
+    result: 0
+  };
+  handleClick_1= () => {
+    alert(1);
   }
-
-  return (
-  <div class = "cal">
-    <div id = "arr1">
-      <button onClick={handleClick("1")}>1</button>
-      <button onClick={handleClick}>2</button>
-      <button onClick={handleClick}>3</button>
-      <button onClick={handleClick}>/</button>
+  handleClick_2= () => {
+    alert(1);
+  }
+  handleClick_3= () => {
+    alert(1);
+  }
+  handleClick_4= () => {
+    alert(1);
+  }
+  handleClick_5= () => {
+    alert(1);
+  }
+  handleClick_6= () => {
+    alert(1);
+  }
+  handleClick_7= () => {
+    alert(1);
+  }
+  handleClick_8= () => {
+    alert(1);
+  }
+  handleClick_9= () => {
+    alert(1);
+  }
+  handleClick_0= () => {
+    alert(1);
+  }
+  render(){
+    const {result} = this.state;
+    const {handleClick_0} = this.handleClick_0
+    return (
+    <div class = "cal">
+      <div id = "arr1">
+        <button onClick={handleClick_0}>1</button>
+        <button onClick={handleClick_0}>2</button>
+        <button onClick={handleClick_0}>3</button>
+        <button onClick={handleClick_0}>/</button>
+      </div>
+      <div id = "arr2">
+        <button onClick={handleClick_0}>4</button>
+        <button onClick={handleClick_0}>5</button>
+        <button onClick={handleClick_0}>6</button>
+        <button onClick={handleClick_0}>x</button>
+      </div>
+      <div id = "arr3">
+        <button onClick={handleClick_0}>7</button>
+        <button onClick={handleClick_0}>8</button>
+        <button onClick={handleClick_0}>9</button>
+        <button onClick={this.handleClick_9}>-</button>
+      </div>
+      <div id = "arr4">
+        <button onClick={handleClick_0}>0</button>
+        <button onClick={handleClick_0}>00</button>
+        <button onClick={handleClick_0}>=</button>
+        <button onClick={handleClick_0}>+</button>
+      </div>
     </div>
-    <div id = "arr2">
-      <button onClick={handleClick}>4</button>
-      <button onClick={handleClick}>5</button>
-      <button onClick={handleClick}>6</button>
-      <button onClick={handleClick}>x</button>
-    </div>
-    <div id = "arr3">
-      <button onClick={handleClick}>7</button>
-      <button onClick={handleClick}>8</button>
-      <button onClick={handleClick}>9</button>
-      <button onClick={handleClick}>-</button>
-    </div>
-    <div id = "arr4">
-      <button onClick={handleClick}>0</button>
-      <button onClick={handleClick}>00</button>
-      <button onClick={handleClick}>=</button>
-      <button onClick={handleClick}>+</button>
-    </div>
-  </div>
-  );
+    );
+  }
 }
 export default App;
